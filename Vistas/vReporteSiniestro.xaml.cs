26cbc55b2b12549c763efcdcee166013455dc96a
pona <e1003327283@uisrael.edu.ec>
2024-05-16 20:42:47 -0500

    pantalla2

---
+++ Vistas/vReporteSiniestro.xaml.cs
@@ -0,0 +1,21 @@
+namespace ProyectoFinal.Vistas;
+
+public partial class vReporteSiniestro : ContentPage
+{
+	public vReporteSiniestro()
+	{
+		InitializeComponent();
+	}
+
+   
+
+    private void btn_camra_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void btnSiniestro_Clicked(object sender, EventArgs e)
+    {
+
+    }
+}
