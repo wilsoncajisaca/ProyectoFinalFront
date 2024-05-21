9d2e7d6681f7160ae9ab3b6c4b64cb548776b214
wcajisaca <wcajisaca@ec.krugercorp.com>
2024-05-20 21:24:48 -0500

    Se realiza la peticion login al back

---
+++ Objetos/ErrorResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinal.Objetos
+{
+    internal class ErrorResponse
+    {
+        public string Message { get; set; }
+    }
+}
---
+++ Objetos/LoginResponse.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace ProyectoFinal.Objetos
+{
+    internal class LoginResponse
+    {
+        [JsonProperty("usuario")]
+        public string Usuario { get; set; }
+
+        [JsonProperty("rol")]
+        public string Rol { get; set; }
+
+        [JsonProperty("token")]
+        public string Token { get; set; }
+    }
+}
